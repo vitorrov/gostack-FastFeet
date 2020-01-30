7270e484dd2121d330938c89a65780a9dddd604b
Vitor <vitormr45@gmail.com>
2020-01-29 21:02:16 -0300

    Recipient controller

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,39 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .integer()
+        .required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already registered' });
+    }
+
+    const { name, street } = await Recipient.create(req.body);
+
+    return res.json({
+      name,
+      street,
+    });
+  }
+}
+
+export default new RecipientController();
