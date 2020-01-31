8feef35ba449eb3539f1c740d097387e6255c299
Vitor <vitormr45@gmail.com>
2020-01-31 01:31:46 -0300

    Minimal updates

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,13 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Distributor from '../app/models/Distributor';
 import Recipient from '../app/models/Recipient';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, File, Distributor, Recipient];
 
 class Database {
   constructor() {
@@ -15,7 +17,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import DistributorController from './app/controllers/DistributorController';
 
 import authMiddleware from './app/middlewares/auth';
 import isAdmin from './app/middlewares/isAdmin';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store); // Criar usuario
 routes.post('/sessions', SessionController.store); // Login
@@ -18,6 +23,10 @@ routes.put('/users', UserController.update); // Atualizar usuario
 
 routes.post('/recipients', isAdmin, RecipientController.store); // Cadastra destinatário
 
+routes.post('/distributors', isAdmin, DistributorController.store);
+
 routes.get('/teste', isAdmin);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
