2d437d68fcbf942003b1040131490b050b492f64
Vitor <vitormr45@gmail.com>
2020-01-31 02:15:33 -0300

    Distributor controller

--- src/app/controllers/DistributorController.js
+++ src/app/controllers/DistributorController.js
@@ -1,12 +1,67 @@
 import Distributor from '../models/Distributor';
 
 class DistributorController {
+  async index(req, res) {
+    const distributors = await Distributor.findAll();
+
+    return res.json(distributors);
+  }
+
   async store(req, res) {
+    const distributorExists = await Distributor.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (distributorExists) {
+      return res.status(401).json({ error: 'Email already registered' });
+    }
+
     const distributor = await Distributor.create(req.body);
 
     return res.json({
       name: distributor.name,
       email: distributor.email,
+    });
+  }
+
+  async update(req, res) {
+    const distributor = await Distributor.findOne({
+      where: { id: req.params.id },
+    });
+
+    const { name, email, avatar_id } = req.body;
+
+    if (name && !(name !== distributor.name)) {
+      return res
+        .status(401)
+        .json({ error: 'Name can not be the same as already registered' });
+    }
+
+    if (email && !(email !== distributor.email)) {
+      return res
+        .status(401)
+        .json({ error: 'Email can not be the same as already registered' });
+    }
+
+    if (avatar_id && !(avatar_id !== distributor.avatar_id)) {
+      return res
+        .status(401)
+        .json({ error: 'Avatar can not be the same as already registered' });
+    }
+    await distributor.update(req.body);
+
+    return res.json(distributor);
+  }
+
+  async destroy(req, res) {
+    const distributor = await Distributor.findOne({
+      where: { id: req.params.id },
+    });
+
+    await distributor.destroy();
+
+    return res.json({
+      success: `Distributor ${distributor.name} was deleted succesfully`,
     });
   }
 }
