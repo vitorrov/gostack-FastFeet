2e244addc42ffbc49c297fdbf0bc63e5b022d579
Vitor <vitormr45@gmail.com>
2020-02-03 19:33:06 -0300

    Jobs

---
+++ src/app/jobs/NewdeliveryMail.js
@@ -0,0 +1,24 @@
+import Mail from '../../lib/Mail';
+
+class NewdeliveryMail {
+  get key() {
+    return 'NewdeliveryMail';
+  }
+
+  async handle({ data }) {
+    const { distributor, order } = data;
+
+    await Mail.sendMail({
+      to: `${distributor.name} <${distributor.email}>`,
+      subject: 'Nova entrega registrada para você!',
+      template: 'new',
+      context: {
+        distributor: distributor.name,
+        order: order.id,
+        product: order.product,
+      },
+    });
+  }
+}
+
+export default new NewdeliveryMail();
